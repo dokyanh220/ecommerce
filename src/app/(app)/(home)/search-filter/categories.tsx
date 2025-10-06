c5522b30d5fcf1fe774f846dd7ff2c1aec2c99a2
dokyanh220 <kyanhdubo2107@gmail.com>
2025-10-06 21:02:53 +0700

    buồn ngủ quá

--- src/app/(app)/(home)/search-filter/categories.tsx
+++ src/app/(app)/(home)/search-filter/categories.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useEffect, useRef, useState } from 'react'
 import { CategoryDropDown } from './categoryDropDown'
 import { CustomCategory } from '../types'
 
@@ -6,6 +9,20 @@ interface Props {
 }
 
 export const Categories = ({ data }: Props) => {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const messureRef = useRef<HTMLDivElement>(null)
+  const viewAllRef = useRef<HTMLDivElement>(null)
+
+  const [visibleCount, setVisibleCount] = useState(data.length)
+  const [isAnyHovered, setIsAnyHovered] = useState(false)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+  const activeCategory = 'all'
+  
+  useEffect(() => {
+
+  }, [])
+   
   return (  
     <div className='relative w-full'>
 
