44bd5cd3ed18bb9b876adc3af3c8ca04a75b18c1
Đỗ Kỳ Anh <164603696+dokyanh220@users.noreply.github.com>
2025-11-10 21:33:27 +0700

    Thêm thành phần trang danh mục và danh mục con

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,7 +1,13 @@
-const Page = () => {
+interface Props{
+  params: Promise<{
+    category: string
+  }>
+}
+const Page = async ({ params }: Props) => {
+  const { category } = await params
   return ( 
-    <div>Category page</div>
-   );
+    <div>Category: {category}</div>
+   )
 }
  
-export default Page;
+export default Page
